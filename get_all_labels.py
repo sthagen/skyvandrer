11cb438bc199eb8660471b2c6df231f171242400
Stefan Hagen <stefan@hagen.link>
2024-03-23 13:32:18 +0100

    Added example client for get-all-labels (paginated) API endpoint
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

---
+++ get_all_labels.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+"""GET - Get all labels.
+
+Returns a paginated list of labels.
+Data Security Policy: Exempt from app access rules
+
+## Scopes
+
+Connect app scope required: READ
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: read:jira-work
+Granular:read:
+    label:jira
+
+"""
+import json
+import os
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+url = f'{API_BASE_URL}/rest/api/3/label'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+response = requests.request('GET', url, headers=headers, auth=auth)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(',', ': ')))
