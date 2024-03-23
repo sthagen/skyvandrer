bdb2b5da4b417330eef9e54c1a7d1c12e8f2cbb1
Stefan Hagen <stefan@hagen.link>
2024-03-23 21:33:33 +0100

    Added example client for get-issue-worklogs API endpoint
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

---
+++ get_issue_worklogs.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+"""GET - Get issue worklogs-
+
+Returns worklogs for an issue, starting from the oldest worklog or from the worklog started on or after a date and time.
+Time tracking must be enabled in Jira, otherwise this operation returns an error.
+For more information, see Configuring time tracking.
+This operation can be accessed anonymously.
+
+Permissions required: Workloads are only returned where the user has:
+
+- Browse projects project permission for the project that the issue is in.
+- If issue-level security is configured, issue-level security permission to view the issue.
+- If the worklog has visibility restrictions, belongs to the group or has the role visibility is restricted to.
+
+Data Security Policy: Not exempt from app access rules
+
+## Scopes
+
+Connect app scope required: READ
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: read:jira-work
+Granular:
+    read:group:jira, read:issue-worklog:jira, read:issue-worklog.property:jira, read:project-role:jira,
+    read:user:jira, read:avatar:jira
+
+"""
+
+import json
+import os
+import sys
+from typing import Union
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+CollectorType = dict[str, Union[bool, int, str, None, dict[str, str], list[object]]]
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+try:
+    issue_id_or_key = sys.argv[1]
+except IndexError as err:
+    raise Exception('missing issue id or key') from err
+
+url = f'{API_BASE_URL}/rest/api/3/issue/{issue_id_or_key}/worklog'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+query = {'startAt': 0}
+
+collector: CollectorType = {
+    'endpoint': url,
+    'query': {k: v for k, v in query.items()},  # type: ignore
+    'start_index': 0,
+    'page_capacity': 0,
+    'total_count': 0,
+    'errors': [],
+    'error_messages': [],
+    'items': [],
+}
+
+response = requests.request('GET', url, headers=headers, params=query, auth=auth)  # type: ignore
+data = json.loads(response.text)
+
+error_messages = data.get('errorMessages', [])
+if error_messages:
+    for entry in error_messages:
+        collector['error_messages'].append(entry)  # type: ignore
+    errors = data.get('errors', [])
+    for entry in errors:
+        collector['errors'].append(entry)  # type: ignore
+else:
+    for entry in data['worklogs']:
+        collector['items'].append(entry)  # type: ignore
+
+collector['page_capacity'] = data['maxResults']
+collector['total_count'] = len(collector['items'])  # type: ignore
+
+print(json.dumps(collector, sort_keys=False, indent=4, separators=(',', ': ')))
