a5eadb1b09bda8b60792a7476a3d49e76bcfc95b
Stefan Hagen <stefan@hagen.link>
2024-03-23 17:50:19 +0100

    Added example clients for find-groups and get-users-from-group (query string requiring) API endpoints
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

---
+++ find_groups.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+"""GET - Find groups.
+
+Returns a list of groups whose names contain a query string.
+A list of group names can be provided to exclude groups from the results.
+
+The primary use case for this resource is to populate a group picker suggestions list.
+To this end, the returned object includes the html field where the matched query term is
+highlighted in the group name with the HTML strong tag.
+Also, the groups list is wrapped in a response object that contains a header for use in the picker,
+specifically Showing X of Y matching groups.
+
+The list returns with the groups sorted. If no groups match the list criteria, an empty list is returned.
+
+This operation can be accessed anonymously.
+Permissions required: Browse projects project permission. Anonymous calls and calls by users without
+the required permission return an empty list.
+
+Browse users and groups global permission. Without this permission,
+calls where query is not an exact match to an existing group will return an empty list.
+Data Security Policy: Exempt from app access rules
+
+## Scopes
+
+Connect app scope required: READ
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: read:jira-user
+Granular:
+    read:group:jira
+"""
+import json
+import os
+import sys
+from typing import Union
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+CollectorType = dict[str, Union[bool, int, str, None, dict[str, str], list[object]]]
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+url = f'{API_BASE_URL}/rest/api/3/groups/picker'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+try:
+    query_string = sys.argv[1]
+except IndexError as err:
+    raise Exception('missing query string') from err
+
+query = {
+    'query': query_string,
+    'caseInsensitive': True,
+}
+
+collector: CollectorType = {
+    'endpoint': url,
+    'query': {k: v for k, v in query.items()},  # type: ignore
+    'total_count': 0,
+    'summary_display': None,
+    'errors': [],
+    'error_messages': [],
+    'items': [],
+}
+
+response = requests.request('GET', url, headers=headers, params=query, auth=auth)  # type: ignore
+data = json.loads(response.text)
+
+error_messages = data.get('errorMessages', [])
+if error_messages:
+    for entry in error_messages:
+        collector['error_messages'].append(entry)  # type: ignore
+    errors = data.get('errors', [])
+    for entry in errors:
+        collector['errors'].append(entry)  # type: ignore
+else:
+    collector['summary_display'] = data.get('header')
+    for entry in data.get('groups'):
+        collector['items'].append(entry)  # type: ignore
+
+collector['total_count'] = len(collector['items'])  # type: ignore
+
+print(json.dumps(collector, sort_keys=False, indent=4, separators=(',', ': ')))
---
+++ get_users_from_group.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+"""GET - Get users from group.
+
+Returns a paginated list of all users in a group.
+Note that users are ordered by username, however the username is not returned in the results due to privacy reasons.
+Permissions required: either of:
+
+- Browse users and groups global permission.
+- Administer Jira global permission.
+
+Data Security Policy: Exempt from app access rules
+
+## Scopes
+
+Connect app scope required: ADMIN
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: manage:jira-configuration
+Granular:
+    read:group:jira, read:user:jira, read:avatar:jira
+"""
+import json
+import os
+import re
+import sys
+from typing import Union
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+CollectorType = dict[str, Union[bool, int, str, dict[str, str], list[object]]]
+
+UUID_PATTERN = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$')
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+url = f'{API_BASE_URL}/rest/api/3/group/member'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+try:
+    group_id_or_name = sys.argv[1]
+except IndexError as err:
+    raise Exception('missing group id or name') from err
+
+# experimental matcher for id shape as 5e1c5ec7-a634-4cd9-887a-618166d49a25
+#                                      012345678
+is_group_id = bool(UUID_PATTERN.match(group_id_or_name.lower()))
+payload_key = 'groupId' if is_group_id else 'groupname'
+
+query = {
+    'startAt': 0,
+    payload_key: group_id_or_name,
+    'includeInactiveUsers': True,
+}
+
+collector: CollectorType = {
+    'endpoint': url,
+    'query': {k: v for k, v in query.items() if k != 'startAt'},  # type: ignore
+    'is_complete': False,
+    'page_capacity': 0,
+    'roundtrip_count': 0,
+    'start_index': 0,
+    'total_count': 0,
+    'errors': [],
+    'error_messages': [],
+    'items': [],
+}
+my_start = 0
+incomplete = True
+
+while incomplete:
+    query['startAt'] = my_start
+    response = requests.request('GET', url, headers=headers, params=query, auth=auth)  # type: ignore
+    data = json.loads(response.text)
+    error_messages = data.get('errorMessages', [])
+    if error_messages:
+        for entry in error_messages:
+            collector['error_messages'].append(entry)  # type: ignore
+        errors = data.get('errors', [])
+        for entry in errors:
+            collector['errors'].append(entry)  # type: ignore
+        break
+
+    total = data['total']
+    if not collector['total_count']:
+        collector['total_count'] = total
+    elif collector['total_count'] != total:
+        raise IndexError(f'initial total_count({collector["total_count"]}) != ({total})')
+
+    max_results = data['maxResults']
+    if not collector['page_capacity']:
+        collector['page_capacity'] = max_results
+    elif collector['page_capacity'] != max_results:
+        raise IndexError(f'initial page_capacity({collector["page_capacity"]}) != ({max_results})')
+
+    for entry in data['values']:
+        collector['items'].append(entry)  # type: ignore
+
+    collector['is_complete'] = data['isLast']
+    incomplete = not collector['is_complete']
+
+    my_start += max_results
+    collector['roundtrip_count'] += 1  # type: ignore
+
+
+print(json.dumps(collector, sort_keys=False, indent=4, separators=(',', ': ')))
