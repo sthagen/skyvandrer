a88484668a72b8256234516f566759bf1dfbf975
Stefan Hagen <stefan@hagen.link>
2024-03-23 14:55:18 +0100

    Added example clients for paginated get-all-(field-configurations,issue-type-schemes) API endpoints
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

---
+++ get_all_field_configurations.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+"""GET - Get all field configurations.
+
+Returns a paginated list of field configurations. The list can be for all field configurations or
+a subset determined by any combination of these criteria:
+
+- a list of field configuration item IDs.
+- whether the field configuration is a default.
+- whether the field configuration name or description contains a query string.
+
+Only field configurations used in company-managed (classic) projects are returned.
+Permissions required: Administer Jira global permission.
+Data Security Policy: Exempt from app access rules
+
+## Scopes
+
+Connect app scope required: ADMIN
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: manage:jira-configuration
+Granular:
+    read:field-configuration:jira
+
+"""
+import json
+import os
+from typing import Union
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+CollectorType = dict[str, Union[bool, int, str, list[object]]]
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+url = f'{API_BASE_URL}/rest/api/3/fieldconfiguration'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+query = {'startAt': 0}
+
+collector: CollectorType = {
+    'endpoint': url,
+    'is_complete': False,
+    'page_capacity': 0,
+    'roundtrip_count': 0,
+    'start_index': 0,
+    'total_count': 0,
+    'items': [],
+}
+my_start = 0
+incomplete = True
+
+while incomplete:
+    query['startAt'] = my_start
+    response = requests.request('GET', url, headers=headers, params=query, auth=auth)
+    data = json.loads(response.text)
+
+    total = data['total']
+    if not collector['total_count']:
+        collector['total_count'] = total
+    elif collector['total_count'] != total:
+        raise IndexError(f'initial total_count({collector["total_count"]}) != ({total})')
+
+    max_results = data['maxResults']
+    if not collector['page_capacity']:
+        collector['page_capacity'] = max_results
+    elif collector['page_capacity'] != max_results:
+        raise IndexError(f'initial page_capacity({collector["page_capacity"]}) != ({max_results})')
+
+    for entry in data['values']:
+        collector['items'].append(entry)  # type: ignore
+
+    collector['is_complete'] = data['isLast']
+    incomplete = not collector['is_complete']
+
+    my_start += max_results
+    collector['roundtrip_count'] += 1  # type: ignore
+
+
+print(json.dumps(collector, sort_keys=False, indent=4, separators=(',', ': ')))
---
+++ get_all_issue_type_schemes.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+"""GET - Get all issue type schemes.
+
+Returns a paginated list of issue type schemes.
+Only issue type schemes used in classic projects are returned.
+Permissions required: Administer Jira global permission.
+Data Security Policy: Exempt from app access rules
+
+## Scopes
+
+Connect app scope required: ADMIN
+OAuth 2.0 scopes required:
+Classic RECOMMENDED: manage:jira-configuration
+Granular:
+    read:issue-type-scheme:jira
+
+"""
+import json
+import os
+from typing import Union
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+CollectorType = dict[str, Union[bool, int, str, list[object]]]
+
+API_BASE_URL = os.getenv('SUHTEITA_BASE_URL', '')
+API_USER = os.getenv('SUHTEITA_USER', '')
+API_TOKEN = os.getenv('SUHTEITA_TOKEN', '')
+
+if not all(value for value in (API_BASE_URL, API_USER, API_TOKEN)):
+    raise KeyError('missing at least one value for API_BASE_URL, API_USER, and API_TOKEN')
+
+url = f'{API_BASE_URL}/rest/api/3/issuetypescheme'
+
+auth = HTTPBasicAuth(API_USER, API_TOKEN)
+
+headers = {'Accept': 'application/json'}
+
+query = {'startAt': 0}
+
+collector: CollectorType = {
+    'endpoint': url,
+    'is_complete': False,
+    'page_capacity': 0,
+    'roundtrip_count': 0,
+    'start_index': 0,
+    'total_count': 0,
+    'items': [],
+}
+my_start = 0
+incomplete = True
+
+while incomplete:
+    query['startAt'] = my_start
+    response = requests.request('GET', url, headers=headers, params=query, auth=auth)
+    data = json.loads(response.text)
+
+    total = data['total']
+    if not collector['total_count']:
+        collector['total_count'] = total
+    elif collector['total_count'] != total:
+        raise IndexError(f'initial total_count({collector["total_count"]}) != ({total})')
+
+    max_results = data['maxResults']
+    if not collector['page_capacity']:
+        collector['page_capacity'] = max_results
+    elif collector['page_capacity'] != max_results:
+        raise IndexError(f'initial page_capacity({collector["page_capacity"]}) != ({max_results})')
+
+    for entry in data['values']:
+        collector['items'].append(entry)  # type: ignore
+
+    collector['is_complete'] = data['isLast']
+    incomplete = not collector['is_complete']
+
+    my_start += max_results
+    collector['roundtrip_count'] += 1  # type: ignore
+
+
+print(json.dumps(collector, sort_keys=False, indent=4, separators=(',', ': ')))
