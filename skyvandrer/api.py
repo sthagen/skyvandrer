0b44b3c5de7fc3f4e27efd0d2292a2cf774662d3
Stefan Hagen <stefan@hagen.link>
2024-03-24 22:20:35 +0100

    Flushed changes queue (missing API trampoline)
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- skyvandrer/api.py
+++ skyvandrer/api.py
@@ -4,6 +4,7 @@ from skyvandrer import API_BASE_URL, API_TOKEN, API_USER, CollectorType
 from skyvandrer.find_groups import find_groups as impl_find_groups
 from skyvandrer.get_audit_records import get_audit_records as impl_get_audit_records
 
+
 def find_groups(
     query_string: str, api_base_url: str = API_BASE_URL, api_user: str = API_USER, api_token: str = API_TOKEN
 ) -> CollectorType:
